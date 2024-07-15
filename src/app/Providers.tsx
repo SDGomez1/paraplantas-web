7a6621644688ef1ab6ad041ab3e3858e239d33a9
Sergio Gomez <sergio.gomez2000@hotmail.com>
2024-07-15 16:15:07 -0500

    Project initialized

---
+++ src/app/Providers.tsx
@@ -0,0 +1,15 @@
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import type { ReactNode } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only",
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+  });
+}
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
